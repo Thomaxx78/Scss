7e69b8405321ae55943fe817803df47aa34dcd81
Thomas Fi <tom.filhol@gmail.com>
2022-10-22 22:41:52 +0200

    Final

---
+++ dark.js
@@ -0,0 +1,19 @@
+const btnToggle = document.querySelector('.btn-toggle');
+
+btnToggle.addEventListener('click', () => {
+
+  const body = document.body;
+
+  if (body.classList.contains('dark')){
+
+    body.classList.add('light')
+    body.classList.remove('dark')
+    btnToggle.innerHTML = "nuit"
+
+  } else if(body.classList.contains('light')){
+
+    body.classList.add('dark')
+    body.classList.remove('light')
+    btnToggle.innerHTML = "jour"
+  }
+})
---
+++ fleche.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+
+    window.scrollTo({
+        top: 900,
+        left: 0,
+        behavior: "smooth"
+    })
+
+})
